fcf61c279330cbcd9d7c1877c4422a95a6074fce
carlos mendez <carlosalcides190304@gmail.com>
2025-05-29 10:59:19 -0400

    i fixed an error in the funcion namw when inporting it

--- Source/ValidateInput.hs
+++ Source/ValidateInput.hs
@@ -1,9 +1,9 @@
+--validar la entrada por pantalla 
+module Source.ValidateInput (validateInput) where
+
 import Data.Char (toLower)
 
---validar la entrada por pantalla 
 
-module Source.ValidateInput (validateInput) where
-    
 --función para validar que el caracter ingresado es valido 
 validateInput :: Char -> Bool
 validateInput char = elem (toLower char) "abcdefghijklmnopqrstuvwxyz"
